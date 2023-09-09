523d5b6a1f40dd82e074c991d5637224e9ef980d
Adesoye <abdulmumeenadesoye19@gmail.com>
2023-09-09 16:49:03 +0100

    Second commit

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import pytz
 
 app = FastAPI()
 
-@app.get('/api/')
+@app.get('/api')
 async def personal_info(slack_name: str=Query(..., description = "Enter your slack name"), track: str=Query(..., description = "Enter your track")):
     utc_time = datetime.now(pytz.utc)
-    response = {"slack name": slack_name, "current_day": utc_time.strftime("%A"), "utc_time": utc_time.strftime("%Y-%m-%dT%H:%M:%SZ"), "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext", "github_repo_url": "https://github.com/username/repo", "track": track, "status_code": 200}
+    response = {"slack name": slack_name, "current_day": utc_time.strftime("%A"), "utc_time": utc_time.strftime("%Y-%m-%dT%H:%M:%SZ"), "github_file_url": "https://github.com/Abdulmumin3/HNG-task-1/blob/main/main.py", "github_repo_url": "https://github.com/Abdulmumin3/HNG-task-1", "track": track, "status_code": 200}
     return response
